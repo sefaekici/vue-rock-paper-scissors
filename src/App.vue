ec1146138553d5184c3e3b99fc2472a3d8f3b7d4
Sefa Ekici <40717655+sefaekici@users.noreply.github.com>
2021-07-25 15:23:14 +0300

    Style for HandOptions component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <app-score-area></app-score-area>
-
-
+    <app-hand-options></app-hand-options>
   </div>
 </template>
 
 <script>
 import ScoreArea from "./components/ScoreArea.vue"
+import HandOptions from "./components/HandOptions.vue"
 export default {
   components:{
     appScoreArea:ScoreArea,
+    appHandOptions:HandOptions
   }
 }
 </script>
---
+++ src/components/HandOptions.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="hand-options">
+        <button><img src="../assets/paper.png" alt="paper"></button>
+        <button><img src="../assets/rock.png" alt="rock"></button>
+        <button><img src="../assets/scissors.png" alt="scissors"></button>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+    .hand-options{
+        margin-top: 2rem;
+        width: 350px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+        button{
+            width: 150px;
+            height: 150px;
+            margin-top: 1rem;
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #F0F5F9;
+            transition: opacity .3s;
+            &:hover{
+                opacity: .7;
+            }
+            &:nth-child(1){
+                margin-right: 1rem;
+                border: 15px solid #303841;
+            }
+            &:nth-child(2){
+                border: 15px solid #07689F;
+            }
+
+            &:nth-child(3){
+                border: 15px solid #1FAB89;
+            }
+        }
+    }
+</style>
